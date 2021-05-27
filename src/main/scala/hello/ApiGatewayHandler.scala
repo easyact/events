b322cc44932a9a22871b445d04316544902268a2
zhaolei <zhaolei@easyact.cn>
2021-05-27 17:00:16 +0800

    + dynamodb

--- src/main/scala/hello/ApiGatewayHandler.scala
+++ src/main/scala/hello/ApiGatewayHandler.scala
@@ -6,16 +6,6 @@ import org.apache.logging.log4j.{LogManager, Logger}
 
 import scala.jdk.CollectionConverters._
 
-class Handler extends RequestHandler[Request, Response] {
-
-  val logger: Logger = LogManager.getLogger(getClass)
-
-  def handleRequest(input: Request, context: Context): Response = {
-    logger.info(s"Received a request: $input")
-    Response("Go Serverless v1.0! Your function executed successfully!", input)
-  }
-}
-
 class ApiGatewayHandler extends RequestHandler[APIGatewayProxyRequestEvent, ApiGatewayResponse] {
   val scalaMapper = {
     import com.fasterxml.jackson.databind.ObjectMapper
@@ -25,9 +15,10 @@ class ApiGatewayHandler extends RequestHandler[APIGatewayProxyRequestEvent, ApiG
 
   def handleRequest(input: APIGatewayProxyRequestEvent, context: Context): ApiGatewayResponse = {
     val headers = Map("x-custom-response-header" -> "my custom response header value")
-    val logger = context.getLogger
     val in = scalaMapper.writeValueAsString(input)
-    logger.log(s"request: $in")
+    context.getLogger.log(s"request: $in")
+    val logger: Logger = LogManager.getLogger(getClass)
+    logger.debug(s"request: $in")
     ApiGatewayResponse(200, in,
       headers.asJava,
       base64Encoded = true)

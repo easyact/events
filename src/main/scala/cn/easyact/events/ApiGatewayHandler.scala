6a70b91dddbe1ad1f8d2f0758b34db12e6ca440e
zhaolei <zhaolei@easyact.cn>
2021-05-28 18:02:36 +0800

    * http gateway no method

--- src/main/scala/cn/easyact/events/ApiGatewayHandler.scala
+++ src/main/scala/cn/easyact/events/ApiGatewayHandler.scala
@@ -8,21 +8,27 @@ import scala.jdk.CollectionConverters._
 import scala.language.implicitConversions
 import scala.sys.env
 
-class ApiGatewayHandler extends RequestHandler[APIGatewayProxyRequestEvent, ApiGatewayResponse] {
+sealed trait ApiGatewayHandler extends RequestHandler[APIGatewayProxyRequestEvent, ApiGatewayResponse] {
   implicit def f[A](eventRepoF: EventRepoF[A]): EventRepo[A] = Free.liftF(eventRepoF)
 
   def handleRequest(req: APIGatewayProxyRequestEvent, context: Context): ApiGatewayResponse = {
     val log: LambdaLogger = context.getLogger
     log.log(s"env: $env")
     log.log(s"req: $req")
-    val cmd = req.getHttpMethod match {
-      case "POST" => StoreJsonSeq(req.getBody)
-      case "GET" => Get(req.getPathParameters.get("email"))
-    }
-    val r = EventRepoDynamoDB(log).apply(cmd).unsafePerformSync
+    val r = EventRepoDynamoDB(log).apply(cmd(req)).unsafePerformSync
     log.log(s"outcome: $r")
     ApiGatewayResponse(200, r.toString,
       Map("x-custom-response-header" -> "my custom response header value").asJava,
       base64Encoded = true)
   }
+
+  def cmd(req: APIGatewayProxyRequestEvent): EventRepo[_]
+}
+
+case class Post() extends ApiGatewayHandler {
+  override def cmd(req: APIGatewayProxyRequestEvent): EventRepo[Any] = StoreJsonSeq(req.getBody)
+}
+
+case class Get() extends ApiGatewayHandler {
+  override def cmd(req: APIGatewayProxyRequestEvent): EventRepo[Any] = GetEvents(req.getPathParameters.get("email"))
 }

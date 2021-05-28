b271c71d96837f498c6f45bb9d6fbd90befa1204
zhaolei <zhaolei@easyact.cn>
2021-05-28 17:35:46 +0800

    * compiler

--- src/main/scala/cn/easyact/events/EventRepoDynamoDB.scala
+++ src/main/scala/cn/easyact/events/EventRepoDynamoDB.scala
@@ -43,7 +43,7 @@ case class EventRepoDynamoDB(log: LambdaLogger) extends EventRepoInterpreter {
         val outcome: BatchWriteItemOutcome = dynamoDB.batchWriteItem(
           new TableWriteItems(tableName).withItemsToPut(items: _*))
         now(outcome)
-      case Get(user) => now(List(user))
+      case Get(user) => now(List())
       case Store(event) => now(dynamoDB.getTable(tableName).putItem(toItem(event)))
     }
   }
